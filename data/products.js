bc59ce06a6f4ccf409010fa0402aa3a36b4d399c
tot437 <sosomakerone@gmail.com>
2026-06-24 18:02:38 +0300

    use inheritance and poiymorphism to add size chart

--- data/products.js
+++ data/products.js
@@ -36,6 +36,33 @@ class Product {
         return `  $ ${formatCurrency(this.priceCents)}`;
     }
 }
+
+class Clothing extends Product {
+    sizeChartLink;
+    constructor(productDetails) {
+        super(productDetails);
+        this.sizeChartLink = productDetails.sizeChartLink;
+        this.type = productDetails.type;
+    }
+}
+const tshirt = new Clothing({
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+        stars: 4.5,
+        count: 56
+    },
+    priceCents: 799,
+    keywords: [
+        "tshirts",
+        "apparel",
+        "mens"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
+});
+console.log(tshirt);
 const product1 = new Product({
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
@@ -713,5 +740,8 @@ export const products = [{
         ]
     }
 ].map((productDetails) => {
+    if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+    };
     return new Product(productDetails);
 });
