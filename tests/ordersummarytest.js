f1f0b540f3aa3e899bb40fac98c076056a54b919
tot437 <sosomakerone@gmail.com>
2026-06-25 15:38:49 +0300

    add url parameters examble

--- tests/ordersummarytest.js
+++ tests/ordersummarytest.js
@@ -2,50 +2,68 @@ import { renderOrderSummary } from '../utills/checkout/ordersummary.js';
 import { loadFromStorage, cards } from '../data/cards.js';
 import { loadProducts } from '../data/products.js';
 
-describe('test suite: render order summary', () => {
+describe('test suite: renderOrderSummary', () => {
     const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
     const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
+
+    beforeAll((done) => {
+        loadProductsFetch().then(() => {
+            done();
+        });
+    });
+
     beforeEach(() => {
         spyOn(localStorage, 'setItem');
+
         document.querySelector('.js-test-container').innerHTML = `
-            <div class="js-order-summary"></div>
-            <div class="js-payment-summary"></div> `;
+      <div class="js-order-summary"></div>
+      <div class="js-payment-summary"></div>
+    `;
 
         spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([{
-                    productID: productId1,
-                    quantity: 2,
-                    deliveryOptionID: '1'
-                },
-                {
-                    productID: productId2,
-                    quantity: 1,
-                    deliveryOptionID: '2'
-                }
-            ]);
+                productId: productId1,
+                quantity: 2,
+                deliveryOptionId: '1'
+            }, {
+                productId: productId2,
+                quantity: 1,
+                deliveryOptionId: '2'
+            }]);
         });
-
         loadFromStorage();
+
         renderOrderSummary();
     });
 
-    it('displays the cart and handles deletion', () => {
-        // إضافة التوقعات (Expectations) داخل الـ it
-        expect(document.querySelector('.js-order-summary').innerHTML).not.toEqual('');
-        expect(document.querySelector('.js-payment-summary').innerHTML).not.toEqual('');
-        expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(2);
+    it('displays the cart', () => {
+        expect(
+            document.querySelectorAll('.js-cart-item-container').length
+        ).toEqual(2);
+        expect(
+            document.querySelector(`.js-product-quantity-${productId1}`).innerText
+        ).toContain('Quantity: 2');
+        expect(
+            document.querySelector(`.js-product-quantity-${productId2}`).innerText
+        ).toContain('Quantity: 1');
 
-        expect(document.querySelector(`.js-product-quantity-${productId1}`).innerText).toContain('Quantity: 2');
-        expect(document.querySelector(`.js-product-quantity-${productId2}`).innerText).toContain('Quantity: 1');
+        document.querySelector('.js-test-container').innerHTML = '';
+    });
 
+    it('removes a product', () => {
         document.querySelector(`.js-delete-link-${productId1}`).click();
+        expect(
+            document.querySelectorAll('.js-cart-item-container').length
+        ).toEqual(1);
+        expect(
+            document.querySelector(`.js-cart-item-container-${productId1}`)
+        ).toEqual(null);
+        expect(
+            document.querySelector(`.js-cart-item-container-${productId2}`)
+        ).not.toEqual(null);
+        expect(cart.length).toEqual(1);
+        expect(cart[0].productId).toEqual(productId2);
 
-        expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(1);
-        expect(document.querySelector(`.js-cart-item-container-${productId1}`)).toEqual(null);
-        expect(document.querySelector(`.js-cart-item-container-${productId2}`)).not.toEqual(null);
-        expect(cards.length).toEqual(1);
-        expect(cards[0].productID).toEqual(productId2);
-
-        document.querySelector('.js-test-container').innerHTML = ` `;
+        document.querySelector('.js-test-container').innerHTML = '';
     });
 });
--- utills/checkout/paymentsummary.js
+++ utills/checkout/paymentsummary.js
@@ -58,9 +58,28 @@ export function renderPaymentSummary() {
                     </div>
                 </div>
 
-                <button class="place-order-button button-primary">
+                <button class="place-order-button button-primary js-place-order">
 						Place your order
 					</button>
     `;
     document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
-}
+    document.querySelector('.js-place-order').addEventListener('click', async() => {
+        try {
+            const response = await fetch('https://supersimplebackend.dev/orders', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    cart: cart
+                })
+            });
+
+            const order = await response.json();
+            addOrder(order);
+            window.location.href = 'orders.html';
+        } catch (error) {
+            console.log('Unexpected error. Try again later.');
+        }
+    })
+};
